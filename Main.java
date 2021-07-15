72dfd880034bffa0877681d3c77b5ea5e227b057
koide28 <84113026+koide28@users.noreply.github.com>
2021-07-15 10:13:42 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package CLASSROOM.Session37;
+
+public class Main{
+    public static void main(String[] args){
+        Game game = new Game();
+
+        //ゲーム初期化
+       game.init();
+        //ゲーム開始
+        game.execute();
+    }
+}
